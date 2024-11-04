1a912c7b9af48001f85b9e1c20398101d92dffab
shafitrisalma <shafitrisalmaaa@gmail.com>
2024-11-04 22:25:26 +0700

    Add files via upload

---
+++ SI_RegPagi_23176029Latihan18Kelipatan.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package si_regpagi_23176029latihan18kelipatan;
+
+/**
+ *
+ * @author shafi
+ * Nama  : Shafitri Salma Ramadanti
+ * NIM   : 23176029
+ * Kelas : PBO1
+ * Prodi : Sistem Informasi
+ * Deskripsi Program : Program ini berisi program untuk menampilkan angka kelipatan
+ */
+public class SI_RegPagi_23176029Latihan18Kelipatan {
+
+    public static void main(String[] args) {
+        double angka = 3.5;
+        while (angka <= 35){
+            System.out.println(angka);
+            angka += 3.5;
+        }
+
+    }
+    
+}
